933def5f3465e669a90f34ccba58e5675ec70691
IvanRomero03 <A00833623@itesm.mx>
2022-03-16 08:54:03 -0600

    Limpieza y organizacion

--- main1.py
+++
@@ -1,9 +0,0 @@
-import kivy
-from kivy.app import App
-from ConectorDB import BaseDeDatos
-from inicioApp import PaginaInicio
-
-class AppMain(App):
-    def build(self):
-        PaginaInicio()
-        
